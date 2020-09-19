569aaca16d8adfdba71331d806b72b75e25de803
ana-ko <akokh18@freeuni.edu.ge>
2020-09-19 18:18:01 +0400

    Add room_configuration.ejs Add methods to room_dao Add mapping to room_configuration

--- database/dao/room_dao.js
+++ database/dao/room_dao.js
@@ -1,7 +1,66 @@
 db = require('../db').getDb()
 
+const verifyRoomCreator = (userName, roomID) => {
+    try{
+        // checks if room and user are mapped
+        if(!verifyUserRoomMapping(userName, roomID)) return false
+        const userID = getUserIDByName(userName)
+        // checks if user is creator of room
+        const room = getRoomByRoomID(roomID)
+        if(room.length > 4){
+            console.log("Error: room lenght shuldn't be more than 4 " + room.length)
+            return false
+        }
+        const roomCreatorID = room[3]
+        if(userID !== roomCreatorID) return false
+        return true
+    }catch(e){
+        console.log(e)
+        console.log("Unexpected error when verifying user " + userName + " to room with ID " + roomID)
+        return false
+    }
+}
 
+const verifyUserRoomMapping = (userName, roomID) => {
+    try{
+        const resultUserID = db.query(
+            "select ru.userID from room_users as ru where ru.roomID = ?;", [roomID]
+        )
+        if(resultUserID.length === 0) return false
+        const userID = getUserIDByName(userName)
+        if(!resultUserID.contains(userID)) return false
+        return true
+    }catch (e) {
+        console.log(e)
+        console.log("Unexpected error when verifying user " + userName + " to room with ID " + roomID)
+        return false
+    }
+}
+
+const getRoomByRoomID = (roomID) => {
+    const resultRoom = db.query(
+        "select * from rooms as r where r.roomID = ?", [roomID]
+    )
+
+}
+
+const getUserIDByName = (userName) => {
+    try{
+        const resultUser = db.query(
+            "select u.ID from users as u where u.username = ? ", [userName])
+        if(resultUser.length === 0) return null
+        if(resultUser.length > 1){
+            console.log("Error: user by " + userName + " is not unique")
+            return null
+        }
+        return resultUser[0]
+    }catch(e){
+        console.log(e)
+        console.log("Unexpected error when searching user " + userName + " by ID")
+        return null
+    }
+}
 
 module.exports = {
-
+    verifyRoomCreator
 }
--- routers/room_router.js
+++ routers/room_router.js
@@ -1,4 +1,5 @@
 var express = require('express')
+var roomdao = require('../database/dao/room_dao')
 var router = express.Router()
 
 router.get('/', (req, res) => {
@@ -14,6 +15,21 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     console.log("POST request received on room, doing nothing for now")
     res.redirect('/homepage')
+})
+
+router.get('/room_configuration', (req, res) => {
+    console.log("POST request received on room_configuration, doing nothing for now")
+    var userName = req.body.username
+    var roomID = req.body.roomID
+    if(roomdao.verifyRoomCreator(userName, roomID))
+        res.render('room_configuration')
+    else res.render('homepage')
+})
+
+router.post('/room_configuration', (req, res) => {
+    console.log("POST request received on room_configuration, doing nothing for now")
+
+    res.render('room_configuration')
 })
 
 module.exports = router
