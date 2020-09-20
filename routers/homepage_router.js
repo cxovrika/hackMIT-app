cd9314a4b094abca3ea0bba06b011ca82541cfd0
Aleksandre Khokhiashvili <alkhok18@freeuni.edu.ge>
2020-09-20 15:42:20 +0400

    pass users of room to view

--- routers/homepage_router.js
+++ routers/homepage_router.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var roomsDao = require('../database/dao/room_dao')
+var usersDao = require('../database/dao/user_dao')
 var router = express.Router()
 
 router.get('/', (req, res) => {
@@ -11,8 +12,17 @@ router.get('/', (req, res) => {
     }
 
     const user = sess.user;
-    const rooms = roomsDao.getUserRooms(user.username);
-    
+    var rooms = roomsDao.getUserRooms(user.username);
+    rooms = rooms.map((room)=> {
+        // console.log(roomsDao.getUsersFromRoom(room.roomID))
+        // console.log(usersDao.getUserByID(room.roomCreatorID))
+        room.users = roomsDao.getUsersFromRoom(room.roomID)
+        room.users.push(usersDao.getUserByID(room.roomCreatorID))
+        return room
+    })
+    // console.log(rooms)
+    // console.log(rooms[0].users)
+    // console.log(rooms[0].users[0].username)
     res.render('homepage_logged', {
         user: user,
         rooms: rooms,
